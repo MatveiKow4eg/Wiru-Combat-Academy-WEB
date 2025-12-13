7a9c2d4a7fef403aef0e0372dde9d674e2f5c86e
Matvei Tsumitsjov <MatveiKow4eg@users.noreply.github.com>
2025-12-13 03:42:33 +0200

    favicon1

--- app.py
+++ app.py
@@ -303,6 +303,15 @@ def create_app():
     @app.route('/robots.txt')
     def robots():
         return send_from_directory('static', 'robots.txt', mimetype='text/plain')
+    
+    @app.route("/favicon.ico")
+    def favicon():
+        return send_from_directory(
+            os.path.join(app.root_path, "static/images/favicon"),
+            "favicon.ico",
+            mimetype="image/vnd.microsoft.icon",
+    )
+
 
     @app.route("/news")
     def news_list():
